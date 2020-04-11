81239299dce0e6ef46727d0e2a4a077900cd7774
carmarin <carlos.alberto.marin@everis.com>
2020-04-10 23:59:56 -0300

    se agrega metodo para guardar una tarea

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import { Lista } from 'src/app/models/lista.model';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -12,12 +13,46 @@ export class Tab1Page {
   listasTodo: Lista[] = [];
 
   constructor(public todoService: TodoService,
-              private router: Router) {
+              private router: Router,
+              private alertCtrl: AlertController) {
     this.listasTodo = todoService.getLista();
   }
 
-  public agregarLista(){
-    this.router.navigateByUrl('tabs/tab1/agregar');
+  public async agregarLista(){
+    // this.router.navigateByUrl('tabs/tab1/agregar');
+
+    const alert = await this.alertCtrl.create({
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+              console.log('Cancelar creación de lista');
+            }
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            console.log('informacion del formulario', data);
+            if (data.titulo.length === 0) {
+              return;
+            }
+            // Crear la lista
+            this.todoService.crearLista(data.titulo);
+          }
+        },
+      ]
+    });
+
+    await alert.present();
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -10,14 +10,34 @@ export class TodoService {
 
   constructor() {
       console.log('Servicio inicializado');
+      this.cargarStorage();
+/*
       const lista1 = new Lista('Recolectar piedras del infinito');
       const lista2 = new Lista('Héroes a desaparecer');
       this.listas.push(lista1, lista2);
-
+*/
       console.log(this.listas);
     }
 
   public getLista() {
     return this.listas;
+  }
+
+  public crearLista(titulo: string) {
+
+    const nuevaListta = new Lista(titulo);
+    this.listas.push(nuevaListta);
+    this.guardarStorage();
+  }
+
+  public guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  public cargarStorage() {
+
+      if  (localStorage.getItem('data')) {
+        this.listas = JSON.parse(localStorage.getItem('data'));
+      }
   }
 }
